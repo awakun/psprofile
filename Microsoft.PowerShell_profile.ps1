dbd31281ef7c043a9b6dce25e356c7bf84fc78c4
Awakun <nope@gmail.com>
2017-02-22 07:55:28 -0800

    Modified prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
+#Module imports
+Import-Module bash-cmdlets
+
 #Alias shortcuts
 # Alias Git
 New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe"
@@ -17,123 +20,22 @@ Set-PSReadlineOption -BellStyle None
 $WinHome = '/mnt/c/Users/worge'
 $BashHome = "C:\Users\worge\AppData\Local\lxss\home\Awakun"
 
-function Invoke-Bash 
-{
-  <#
-      .SYNOPSIS
-      Runs a bash command
-
-      .DESCRIPTION
-      Invoke Bash attempts to run the provided $command string as a command through the WSL subsystem
-
-      .PARAMETER Command
-      (Mandatory) A string containing the command to run.
-
-      .EXAMPLE
-      Invoke-Bash "sudo apt update"
-      This would update the WSL by running sudo apt update via bash
-    #>
-    Param
-    (
-      [Parameter(
-        Mandatory = $true,
-        Position = 0
-      )]
-      [string]
-      $Command
-    )
-    bash -c "$Command"
-}
-
-function ssh {
-    <#
-      .SYNOPSIS
-      Opens a remote shell connection to $RemoteMachine
-
-      .DESCRIPTION
-      The ssh function opens a remote shell connection using either bash -c "ssh $RemoteMachine"
-      to open an ssh tunnel to a UNIX remote machine, or using Enter-PSSession for a Windows remote
-      machine whose hostname is provided by the $RemoteMachine parameter.
-
-      .PARAMETER RemoteMachine
-      (Mandatory) A single remote machine hostname. Must include full FQDN.
-
-      .PARAMETER UNIX
-      (Optional, Default) Flags the ssh function to use BASH to ssh into the remote UNIX-based computer.
-
-      .PARAMETER Windows
-      (Optional) Allows user to remote into a Windows powershell session instead using Enter-PSSession
-
-      .PARAMETER Credential
-      (Mandatory with Windows parameter) Accepts either a username or a PSCredential object for authentication in PSSession
-
-      .EXAMPLE
-      Remote into a UNIX machine
-      ssh -RemoteMachine ubuntu22.ant.amazon.com
-
-      .EXAMPLE
-      Remote into a Windows machine
-      ssh -Windows -Credential username -RemoteMachine SEA-1800010000.ant.amazon.com
-    #>
-
-    [CmdletBinding(DefaultParameterSetName = 'UNIX')]
-    Param(
-        [Parameter(Mandatory=$true,ParameterSetName = 'UNIX')]
-        [Parameter(Mandatory=$true,ParameterSetName = 'Windows',Position=0)]
-        [Alias("RemoteComputer","ComputerName")]
-        [string]$RemoteMachine,
-        
-        [Parameter(ParameterSetName = 'UNIX')]
-        [Alias("Linux","Mac")]
-        [switch]$UNIX,
-
-        [Parameter(ParameterSetName = 'UNIX')]
-        [Parameter(ParameterSetName = 'WithKey')]
-        [Alias("i")]
-        [string]$Key = $null,
-        
-        [Parameter(ParameterSetName = 'UNIX')]
-        [Parameter(ParameterSetName = 'WithKey', Mandatory = $true)]
-        [string]$User,
-        
-        [Parameter(ParameterSetName = 'Windows')]
-        [switch]$Windows,
-        
-        [Parameter(Mandatory=$true,ParameterSetName = 'Windows')]
-        [System.Management.Automation.CredentialAttribute()]
-        $Credential
-    )
-
-    Process { 
-      if (Test-Connection -ComputerName $RemoteMachine -Count 1 -ErrorAction SilentlyContinue) {    #Test if $RemoteMachine can be pinged.
-          if ($Windows) { #Check for Windows flag
-              Enter-PSSession -ComputerName $RemoteMachine -Credential $Credential
-          } Else { #UNIX style
-              if ($Key) {
-                  bash -c "ssh -i $Key $User@$RemoteMachine"
-              } Else {
-                  bash -c "ssh $RemoteMachine"
-              }
-          }
-      } Else {
-          Write-Error "$RemoteMachine appears to be offline. Verify you are using the FQDN and that the hostname was entered correctly before retrying."
-      }
-    }
-}
-
-function Update-Bash {
-    <#
-      .SYNOPSIS
-      Updates packages for the WSL
-
-      .DESCRIPTION
-      This function will update the package cache (apt update) and then upgrade and packages that need it (apt upgrade)
-
-      .EXAMPLE
-      Update packages
-      Update-BASH
-    #>
-    & bash -c 'sudo apt update && sudo apt upgrade -y'
+#Prompt customization
+function prompt {
+  $global:CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $h, $User = $global:CurrentUser.Name -split '\\'
+  $principal = New-Object System.Security.Principal.WindowsPrincipal($global:CurrentUser)
+  
+  if ($principal.IsInRole("Administrator")) { 
+    $host.ui.rawui.WindowTitle = $CurrentUser.Name + ".Administrator Line: " + $host.UI.RawUI.CursorPosition.Y
+  }
+  else { 
+    $host.ui.rawui.WindowTitle = $CurrentUser.Name + " Line: " + $host.UI.RawUI.CursorPosition.Y
+  }
+  
+  $promptstring = "PS $User@" + $(Get-Item -Path .\).Name + ">"
+  Write-Host $promptstring -NoNewline -ForegroundColor Cyan
+  return " "
 }
 
 # Start a transcript
